97bbae1130d55c3d7f2f051b53f752fdeb157f9e
unknown <avrumiesafranovitz@gmail.com>
2022-05-05 23:05:38 -0400

    added start and end game, and timer

--- develop/script.js
+++ develop/script.js
@@ -1,118 +1,125 @@
-var timer;
-var testTime = 5;
-var score = 0;
+var questionEl = document.querySelector("#question");
+var optionListEl = document.querySelector("#option-list");
+var questionResultEl = document.querySelector("#question-result");
+var timerEl = document.querySelector("#timer");
+var startButtonEl = document.querySelector("#btns");
+var quizBoxEl = document.querySelector("section");
+
 var questionIndex = 0;
-var timerEl = document.getElementById("timer");
-var questionsEl = document.getElementById("questions");
-timerEl.innerText = `Time: ${testTime}`;
-var questionsData = [
+var correctCount = 0;
+
+var time = 30;
+var intervalId;
+
+var questions = [
   {
     question: "Which of these cryptocurrencies is the oldest?",
-    choices: {
-      A: "Ether",
-      B: "Tether",
-      C: "Bicoin",
-    },
-    answer: "C",
+    choices: ["Ether", "Tether", "Bicoin"],
+    answer: "Bitcoin",
   },
   {
     question: "What is a CBDC??",
-    choices: {
-      A: "Magic Intenet Money",
-      B: "Reward for ledning BTC",
-      C: "Digital currency issued by a central bank",
-    },
-    answer: "C",
+    choices: [
+      "Magic Intenet Money",
+      "Reward for ledning BTC",
+      "Digital currency issued by a central bank",
+    ],
+    answer: "Digital currency issued by a central bank",
   },
   {
     question: "What is the worlds most actively traded crypto coin?",
-    choices: {
-      A: "Bitcoin",
-      B: "Tether",
-      C: "Dogecoin",
-    },
-    answer: "B",
+    choices: ["Bitcoin", "Tether", "Dogecoin"],
+    answer: "Tether",
   },
   {
     question: "Where do you store cryptocurrency?",
-    choices: {
-      A: "Blockchain",
-      B: "Chain Wallet",
-      C: "Private Keys",
-    },
-    answer: "A",
+    choices: ["Blockchain", "Chain Wallet", "Private Keys"],
+    answer: "Blockchain",
   },
   {
     question: "What is a smart contract?",
-    choices: {
-      A: "A cloud based contract",
-      B: "A computer protocol",
-      C: "A contract written by lawyers",
-    },
-    answer: "B",
+    choices: [
+      "A cloud based contract",
+      "A computer protocol",
+      "A contract written by lawyers",
+    ],
+    answer: "A computer protocol",
   },
   {
     question: "Who invented Bitcoin?",
-    choices: {
-      A: "The magic wizard",
-      B: "US government",
-      C: "Satoshi Nakamoto",
-    },
-    answer: "C",
+    choices: ["The magic wizard", "US government", "Satoshi Nakamoto"],
+    answer: "Satoshi Nakamoto",
   },
   {
     question: "Who invented Ether?",
-    choices: {
-      A: "Vitalik Buterin",
-      B: "Stan Lee",
-      C: "Tom Hanks",
-    },
-    answer: "A",
+    choices: ["Vitalik Buterin", "Stan Lee", "Tom Hanks"],
+    answer: "Vitalik Buterin",
   },
   {
     question:
       "Which site run by Ross Ulbricht was closed by the FBI for letting people buy drugs with Bitcoin??",
-    choices: {
-      A: "Dark Alley",
-      B: "Silk Road",
-      C: "Silk Street",
-    },
-    answer: "B",
-  },
-  {
-    question:
-      "What is the name of the famous Bitcoin exchange from Japan that collapsed in 2014?",
-    choices: {
-      A: "Blockchain.info",
-      B: "Tradehill",
-      C: "Mt. Gox",
-    },
-    answer: "C",
-  },
-  {
-    question: "What does the block in the blockchain consist of?",
-    choices: {
-      A: "Transaction data",
-      B: "A Hash point",
-      C: "All of these",
-    },
-    answer: "C",
+    choices: ["Dark Alley", "Silk Road", "Silk Street"],
+    answer: "Silk Road",
   },
 ];
 
-document.getElementById("enter").addEventListener("click", () => {
-  startGame();
-  timer = setInterval(() => {
-    testTime--;
-    timerEl.innerText = `Time: ${testTime}`;
-  }, 1000);
-});
-function startGame() {
-  document.getElementById("welcome-page").classList.add("hide");
-  questionsEl.classList.remove("hide");
+function updateTime() {
+  time--;
+  timerEl.textContent = time;
+}
+
+function startQuiz() {
+  startButtonEl.classList.add("hide");
+  quizBoxEl.classList.remove("hide");
+}
+
+function renderQuestion() {
+  if (time == 0) {
+    updateTime();
+    return;
+  }
+
+  intervalId = setInterval(updateTime, 1000);
+
+  questionEl.textContent = questions[questionIndex].question;
+
+  optionListEl.innerHTML = "";
+  questionResultEl.innerHTML = "";
+
+  var choices = questions[questionIndex].choices;
+  var choicesLenth = choices.length;
+
+  for (var i = 0; i < choicesLenth; i++) {
+    var questionListItem = document.createElement("button");
+    questionListItem.textContent = choices[i];
+    optionListEl.append(questionListItem);
+  }
 }
 
 function nextQuestion() {
-  questionsEl.innerHTML = `
-  <h3> ${questionsData[questionIndex].question} </h3>`;
+  questionIndex++;
+  if (questionIndex === questions.length) {
+    time = 0;
+  }
+  renderQuestion();
 }
+
+function checkAnswer(event) {
+  clearInterval(intervalId);
+  if (event.target.matches("button")) {
+    var answer = event.target.textContent;
+    if (answer === questions[questionIndex].answer) {
+      questionResultEl.textContent = "Correct";
+      correctCount++;
+    } else {
+      questionResultEl.textContent = "Incorrect";
+      time = time - 1;
+      timerEl.textContent = time;
+    }
+  }
+  setTimeout(nextQuestion, 2000);
+}
+
+optionListEl.addEventListener("click", checkAnswer);
+startButtonEl.addEventListener("click", startQuiz);
+startButtonEl.addEventListener("click", renderQuestion);
