7461849a621839eea0ac0f0970ebc4da5a1158c5
unknown <avrumiesafranovitz@gmail.com>
2022-05-05 20:11:30 -0400

    added questions

--- develop/script.js
+++ develop/script.js
@@ -7,22 +7,96 @@ var questionsEl = document.getElementById("questions");
 timerEl.innerText = `Time: ${testTime}`;
 var questionsData = [
   {
-    question: "What is my name?",
+    question: "Which of these cryptocurrencies is the oldest?",
     choices: {
-      A: "Allen",
-      B: "Bernard",
-      C: "Charles",
+      A: "Ether",
+      B: "Tether",
+      C: "Bicoin",
+    },
+    answer: "C",
+  },
+  {
+    question: "What is a CBDC??",
+    choices: {
+      A: "Magic Intenet Money",
+      B: "Reward for ledning BTC",
+      C: "Digital currency issued by a central bank",
+    },
+    answer: "C",
+  },
+  {
+    question: "What is the worlds most actively traded crypto coin?",
+    choices: {
+      A: "Bitcoin",
+      B: "Tether",
+      C: "Dogecoin",
+    },
+    answer: "B",
+  },
+  {
+    question: "Where do you store cryptocurrency?",
+    choices: {
+      A: "Blockchain",
+      B: "Chain Wallet",
+      C: "Private Keys",
     },
     answer: "A",
   },
   {
-    question: "What is Bitcoin?",
+    question: "What is a smart contract?",
     choices: {
-      A: "Magic Intenet Money",
-      B: "Cash me outside",
-      C: "The future",
+      A: "A cloud based contract",
+      B: "A computer protocol",
+      C: "A contract written by lawyers",
     },
     answer: "B",
+  },
+  {
+    question: "Who invented Bitcoin?",
+    choices: {
+      A: "The magic wizard",
+      B: "US government",
+      C: "Satoshi Nakamoto",
+    },
+    answer: "C",
+  },
+  {
+    question: "Who invented Ether?",
+    choices: {
+      A: "Vitalik Buterin",
+      B: "Stan Lee",
+      C: "Tom Hanks",
+    },
+    answer: "A",
+  },
+  {
+    question:
+      "Which site run by Ross Ulbricht was closed by the FBI for letting people buy drugs with Bitcoin??",
+    choices: {
+      A: "Dark Alley",
+      B: "Silk Road",
+      C: "Silk Street",
+    },
+    answer: "B",
+  },
+  {
+    question:
+      "What is the name of the famous Bitcoin exchange from Japan that collapsed in 2014?",
+    choices: {
+      A: "Blockchain.info",
+      B: "Tradehill",
+      C: "Mt. Gox",
+    },
+    answer: "C",
+  },
+  {
+    question: "What does the block in the blockchain consist of?",
+    choices: {
+      A: "Transaction data",
+      B: "A Hash point",
+      C: "All of these",
+    },
+    answer: "C",
   },
 ];
 
